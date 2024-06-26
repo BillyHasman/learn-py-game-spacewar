b7bdcaa8cbc018bf5dc6ce50e234d472ce87eff0
Billy Hasman <billyhasman08@gmail.com>
2024-06-26 22:14:47 +0700

    Add_enemy spawn logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
+import random
 
-# Intialize the pygame
+# Initialize the pygame
 pygame.init()
 
 # Create the Screen Size 1366 x 788
@@ -19,19 +20,26 @@ playerY = 550
 # New width and height for player will be (40, 40)
 player_resize = pygame.transform.scale(playerImg, (40, 40))
 
+# Enemy
+playerImg = pygame.image.load('Assets/img/enemy.png')
+enemyX = random.randint(0,1323)
+enemyY = 50
 
-# Function
-def player(x,y):
-    # screen.blit(playerImg, (playerX,playerY)) 
-    screen.blit(player_resize, (x,y)) # Drop player to screenGame
-    
+enemy_resize = pygame.transform.scale(playerImg, (60, 60))
+
+# Function Player
+def player(x, y):
+    screen.blit(player_resize, (x, y))  # Drop player to screenGame
+
+# Function Enemy
+def enemy(x, y):
+    screen.blit(enemy_resize, (x, y))  # Drop enemy to screenGame
 
 # Game Loop
 running = True
 while running:
-
     # Add blackBG
-    screen.fill((0,0,0))
+    screen.fill((0, 0, 0))
 
     speedX = 0.3
     speedY = 0.2
@@ -39,23 +47,17 @@ while running:
     playerX += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * speedX
     playerY += (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * speedY
 
-    print(playerX, playerY)
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     # Boundaries player inside screen
-    if playerX <= 0:
-        playerX = 0
-    elif playerX >=1323:
-        playerX = 1323
+    playerX = max(0, min(playerX, 1323))
+    playerY = max(0, min(playerY, 750))
 
-    if playerY <= 0:
-        playerY = 0
-    elif playerY >=750:
-        playerY = 750
-
-
-    player(playerX,playerY)
+    player(playerX, playerY)
+    
+    enemy(enemyX, enemyY)
     pygame.display.update()
+
+pygame.quit()
