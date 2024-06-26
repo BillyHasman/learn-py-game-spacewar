1a5ea6cb7cb711eb7d7dbcab6fa5b0ed758ea7a6
Billy Hasman <billyhasman08@gmail.com>
2024-06-26 21:16:17 +0700

    add_Screen Size and GameLoop event

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import pygame
+
+# Intialize the pygame
+pygame.init()
+
+# Create the Screen Size 800 x 600
+screen = pygame.display.set_mode((800,600))
+
+# Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
