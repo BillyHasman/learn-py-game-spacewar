19038fee9f6f623a84d87dfaee3c0817b3f1710e
Billy Hasman <billyhasman08@gmail.com>
2024-06-27 05:50:24 +0700

    Add_Enemy Movement and Background

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ pygame.init()
 # Create the Screen Size 1366 x 788
 screen = pygame.display.set_mode((1366 ,788))
 
+# Init Background
+background = pygame.image.load('Assets/img/bg_main.jpg')
+background = pygame.transform.scale(background, (1366, 788))
+
 # Title and Icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('Assets/img/spaceship.png')
@@ -16,6 +20,8 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('Assets/img/player.png')
 playerX = 645 
 playerY = 550
+speedX = 0.4
+speedY = 0.3
 
 # New width and height for player will be (40, 40)
 player_resize = pygame.transform.scale(playerImg, (40, 40))
@@ -24,6 +30,8 @@ player_resize = pygame.transform.scale(playerImg, (40, 40))
 playerImg = pygame.image.load('Assets/img/enemy.png')
 enemyX = random.randint(0,1323)
 enemyY = 50
+enemyX_change = 0.4
+enemyY_change = 15
 
 enemy_resize = pygame.transform.scale(playerImg, (60, 60))
 
@@ -38,11 +46,13 @@ def enemy(x, y):
 # Game Loop
 running = True
 while running:
+
     # Add blackBG
     screen.fill((0, 0, 0))
 
-    speedX = 0.3
-    speedY = 0.2
+    # background Image
+    screen.blit(background, (0,0))
+
     keys = pygame.key.get_pressed()
     playerX += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * speedX
     playerY += (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * speedY
@@ -56,7 +66,17 @@ while running:
     playerY = max(0, min(playerY, 750))
 
     player(playerX, playerY)
-    
+
+    # Enemy Movement
+    enemyX += enemyX_change
+
+    # Check boundaries and update direction and position
+    if enemyX <= 0 or enemyX >= 1323:
+        enemyX_change *= -1
+        enemyY += enemyY_change
+
+
+
     enemy(enemyX, enemyY)
     pygame.display.update()
 
