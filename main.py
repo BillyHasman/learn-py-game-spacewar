4aed9374584788582a7110c2ed2b4833e21111fd
Billy Hasman <billyhasman08@gmail.com>
2024-06-26 21:37:57 +0700

    Add_Player Assets and Display Player

--- main.py
+++ main.py
@@ -3,21 +3,39 @@ import pygame
 # Intialize the pygame
 pygame.init()
 
-# Create the Screen Size 800 x 600
-screen = pygame.display.set_mode((800,600))
+# Create the Screen Size 1366 x 788
+screen = pygame.display.set_mode((1366 ,788))
 
 # Title and Icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('Assets/img/spaceship.png')
 pygame.display.set_icon(icon)
 
+# Player 
+playerImg = pygame.image.load('Assets/img/player.png')
+playerX = 645 
+playerY = 450
+
+# New width and height for player will be (40, 40)
+img_resize = pygame.transform.scale(playerImg, (40, 40))
+
+# Function
+def player():
+    # screen.blit(playerImg, (playerX,playerY)) # Drop player to screenGame
+    screen.blit(img_resize, (playerX,playerY))
+    
+
 # Game Loop
 running = True
 while running:
+
+    # Add blackBG
+    screen.fill((0,0,0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # RGB - Red, Green, Blue
-    screen.fill((255,0,0))
+
+    player()
     pygame.display.update()
